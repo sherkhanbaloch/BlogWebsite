34bd86ecdc2e62d4d69fecc21e5f0bddd9ed70a7
sherkhanbaloch <sherbalochcs@gmail.com>
2025-01-23 18:30:20 +0500

    added code of sending data with post method.

--- BlogWebsite/Controllers/HomeController.cs
+++ BlogWebsite/Controllers/HomeController.cs
@@ -9,12 +9,31 @@ namespace BlogWebsite.Controllers
             return View();
         }
 
-        [HttpGet("{Name}/{FName}")]
-        [Route("Home/Welcome")]
-        public IActionResult Welcome(string? Name, string? FName)
+        //[HttpGet("{Name}/{FName}")]
+        //[Route("Home/Welcome")]
+        //public IActionResult Welcome(string? Name, string? FName)
+        //{
+        //    TempData["Info"] = ("Your Name Is " + Name + " and Father Name Is " + FName);
+        //    return View();
+        //}
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Welcome(string Name, string FName)
+        //{
+        //    TempData["Info"] = ("Your Name Is " + Name + " and Father Name Is " + FName);
+        //    return View();
+        //}
+
+        [HttpPost]
+        public IActionResult MyData(string Name, string FName)
         {
             TempData["Info"] = ("Your Name Is " + Name + " and Father Name Is " + FName);
-            return View();
+            return View("Welcome");
         }
 
     }
