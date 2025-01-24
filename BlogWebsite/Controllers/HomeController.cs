cc5d144c713f22eb47672f661407172372c33fa2
sherkhanbaloch <sherbalochcs@gmail.com>
2025-01-24 18:20:17 +0500

    end of the project

--- BlogWebsite/Controllers/HomeController.cs
+++ BlogWebsite/Controllers/HomeController.cs
@@ -1,11 +1,38 @@
-﻿using BlogWebsite.ViewModels;
+﻿using BlogWebsite.Data;
+using BlogWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogWebsite.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly AppDbContext db;
+        public HomeController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Students.Add(student);
+                db.SaveChanges();
+            }
+
+            TempData["Message"] = "Data Saved Successfully.";
+
+            return View();
+        }
+
+        public IActionResult Welcome()
         {
             return View();
         }
@@ -17,11 +44,6 @@ namespace BlogWebsite.Controllers
         //    TempData["Info"] = ("Your Name Is " + Name + " and Father Name Is " + FName);
         //    return View();
         //}
-
-        public IActionResult Welcome()
-        {
-            return View();
-        }
 
         //[HttpPost]
         //public IActionResult Welcome(string Name, string FName)
@@ -37,12 +59,11 @@ namespace BlogWebsite.Controllers
         //    return View("Welcome");
         //}
 
-        [HttpPost]
-        public IActionResult MyData(Student student)
-        {
-            TempData["Info"] = ("Your Name Is " + student.Name + " and Father Name Is " + student.FName + " and your Mobile Number Is " + student.Mobile);
-            return View("Welcome");
-        }
-
+        //[HttpPost]
+        //public IActionResult MyData(Student student)
+        //{
+        //    TempData["Info"] = ("Your Name Is " + student.Name + " and Father Name Is " + student.FName + " and your Mobile Number Is " + student.Mobile);
+        //    return View("Welcome");
+        //}
     }
 }
